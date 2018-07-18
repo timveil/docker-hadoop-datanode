db75bab39382d6d784526c6f3a3f47c26f8bee42
Tim Veil <tjveil@gmail.com>
2018-07-18 12:11:36 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # docker-hadoop-datanode
+
+docker build --no-cache -t timveil/docker-hadoop-datanode:latest .
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+if [ ! -d $datadir ]; then
+  echo "Datanode data directory not found: $datadir"
+  exit 2
+fi
+
+# why is this needed
+ulimit -c unlimited
+
+$HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR datanode
+
+# why is this needed
+cat /hs_err*
