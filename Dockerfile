df9f757cb728b7dfa9368a3fa2a3433e4f469e9c
Tim Veil <tjveil@gmail.com>
2018-07-28 21:13:00 -0400

    removing custom datanode dir

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,6 @@ LABEL maintainer="tjveil@gmail.com"
 
 HEALTHCHECK CMD curl -f http://localhost:50075/ || exit 1
 
-#todo can this ENV and volume definition be moved to compose?
-ENV HDFS_CONF_dfs_datanode_data_dir=file:///hadoop/dfs/data
-RUN mkdir -p /hadoop/dfs/data
-VOLUME /hadoop/dfs/data
-
 ADD run.sh /run.sh
 RUN chmod a+x /run.sh
 
--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
-
-echo "Datanode data directory is: $datadir"
-
-if [ ! -d $datadir ]; then
-  echo "Datanode data directory not found: $datadir"
-  exit 2
-fi
-
-$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
+echo "Starting Datanode!"
+${HADOOP_HOME}/bin/hdfs --config ${HADOOP_CONF_DIR} datanode
