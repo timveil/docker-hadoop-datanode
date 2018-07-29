ed02cae1bf51210cff4890de0cac2d303c939436
Tim Veil <tjveil@gmail.com>
2018-07-28 21:06:19 -0400

    adding echo

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+
+echo "Datanode data directory is: $datadir"
+
 if [ ! -d $datadir ]; then
   echo "Datanode data directory not found: $datadir"
   exit 2
